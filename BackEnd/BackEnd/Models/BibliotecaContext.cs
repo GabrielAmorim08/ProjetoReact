85b6ee01626f9fa8134eac03b0f3b24805bd36b3
GabrielAmorim08 <gabriel.amorim.08@hotmail.com>
2024-10-23 23:36:39 -0300

    Iniciando conexão com o banco e criando a model de usuario

---
+++ BackEnd/BackEnd/Models/BibliotecaContext.cs
@@ -0,0 +1,19 @@
+﻿using BackEnd.Models.Usuarios;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models
+{
+    public class BibliotecaContext : DbContext
+    {
+        BibliotecaContext(DbContextOptions<BibliotecaContext> context) : base(context) {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var connectionString = builder["ConnectionStrings:bibliotecaContext"];
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ BackEnd/BackEnd/Models/Usuarios/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEnd.Models.Usuarios
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime? Data_Nascimento { get; set; }
+    }
+}
